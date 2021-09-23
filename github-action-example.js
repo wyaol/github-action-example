d00eb2b9e72b159d07ad01da6605a104c4bed8ae
wyao <yaowang@melco-resorts.com>
2021-09-23 13:24:47 +0800

    example2

--- github-action-example.js
+++ github-action-example.js
@@ -6,7 +6,7 @@ const port = 3015
 const server = http.createServer((req, res) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
-  res.end('welcome to www.xxx.com\n')
+  res.end('welcome to www.aaa.com\n')
 })
 
 server.listen(port, hostname, () => {
